8bf196f9b24438fbb91c25b5333831d57115d681
bzqweiyi <bzqweiyi@163.com>
2019-07-04 14:56:02 +0800

    update framwork

---
+++ biLSTM_CRF.py
@@ -0,0 +1,21 @@
+# coding:utf-8
+# James
+# 20190705
+import torch
+
+
+class biLSTM_CRF
+    def __init(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backword(self):
+        pass
+    
+    def loss(self):
+        pass
+
+    def train(self):
+        pass
